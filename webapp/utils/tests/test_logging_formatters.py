07d72461ad9d64e21729b09e932c636e57f7e52f
yuu-eguci <yuu.eguci@gmail.com>
2023-10-28 21:02:09 +0900

    🚨 mypy [annotation-unchecked] を解消

--- webapp/utils/tests/test_logging_formatters.py
+++ webapp/utils/tests/test_logging_formatters.py
@@ -23,19 +23,19 @@ class TestJSTFormatter(unittest.TestCase):
         # msecs を上書き。
         cls.record.msecs = 0
 
-    def test_default_formatter(self):
+    def test_default_formatter(self) -> None:
         # デフォルトの Formatter が作成するログメッセージを確認。
         formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
         formatted_message: str = formatter.format(self.record)
         self.assertEqual(formatted_message, "[2023-02-07 00:00:00,000] [DEBUG] Sample message.")
 
-    def test_JSTFormatter(self):
+    def test_JSTFormatter(self) -> None:
         # JSTFormatter は自動で JST で表示することを確認。
         formatter = JSTFormatter('[%(asctime)s] [%(levelname)s] %(message)s')
         formatted_message: str = formatter.format(self.record)
         self.assertEqual(formatted_message, "[2023-02-07 09:00:00,000] [DEBUG] Sample message.")
 
-    def test_JSTFormatter_with_datefmt(self):
+    def test_JSTFormatter_with_datefmt(self) -> None:
         # まあ、 datefmt と一緒に使うことを想定しているので、そのテストも。
         formatter = JSTFormatter('[%(asctime)s] [%(levelname)s] %(message)s', datefmt='%Y-%m-%dT%H:%M:%S%z')
         formatted_message: str = formatter.format(self.record)
@@ -56,13 +56,13 @@ class TestUTCFormatter(unittest.TestCase):
         # msecs を上書き。
         cls.record.msecs = 0
 
-    def test_UTCFormatter(self):
+    def test_UTCFormatter(self) -> None:
         # UTCFormatter は自動で UTC で表示することを確認。
         formatter = UTCFormatter('[%(asctime)s] [%(levelname)s] %(message)s')
         formatted_message: str = formatter.format(self.record)
         self.assertEqual(formatted_message, "[2023-02-07 00:00:00,000] [DEBUG] Sample message.")
 
-    def test_UTCFormatter_with_datefmt(self):
+    def test_UTCFormatter_with_datefmt(self) -> None:
         # まあ、 datefmt と一緒に使うことを想定しているので、そのテストも。
         formatter = UTCFormatter('[%(asctime)s] [%(levelname)s] %(message)s', datefmt='%Y-%m-%dT%H:%M:%SZ')
         formatted_message: str = formatter.format(self.record)
