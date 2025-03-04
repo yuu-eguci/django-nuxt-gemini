dc2ebfb0535dd1bd5bb5ea487fbaf05f4c5d4752
yuu-eguci <yuu.eguci@gmail.com>
2025-03-04 20:14:58 +0900

    ✨ Django は async が苦手らしい、という噂をきいたんで async view に挑戦

--- webapp/app/urls.py
+++ webapp/app/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # 関数ベース @api_view で実装した View。
+    # djangorestframework 関数ベース @api_view で実装した View。
     path('foo', views.foo_view),
-    # クラスベース APIView で実装した View。
+    # djangorestframework クラスベース APIView で実装した View。
     path('bar', views.BarView.as_view()),
+    # djangorestframework クラスベース APIView で実装した async View。
+    path('baz', views.BazView.as_view()),
 ]
